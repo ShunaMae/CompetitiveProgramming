ff5f73b0694a4ed522f4ad574607c730e5f92a50
ShunaMae <shuna120700@gmail.com>
2024-01-02 13:15:34 +0800

    updated on a09

--- Daily_Practice/鉄則本/A09-WinterinALGOKingdom.py
+++ Daily_Practice/鉄則本/A09-WinterinALGOKingdom.py
@@ -10,7 +10,28 @@ def accumulative_sum_2d(g):
     
     return A
 
+def main():
+    H, W, N = map(int, input().split())
+    G = [[(0) for _ in range(W)] for _ in range(H)]
+    for _ in range(N):
+        a, b, c, d = map(int, input().split())
+        a -= 1
+        b -= 1
+        c -= 1
+        d -= 1 
+        G[a][b] += 1
+        # top right
+        if d<W-1: G[a][d+1] -= 1
+        # bottom left 
+        if c<H-1: G[c+1][b] -= 1
+        # bottom right 
+        if d<W-1 and c<H-1: G[c+1][d+1] += 1
+    print(G)
+    ans = accumulative_sum_2d(G)
+    for row in range(len(G)):
+        print(*ans[row])
 
+main()
 
 
 
