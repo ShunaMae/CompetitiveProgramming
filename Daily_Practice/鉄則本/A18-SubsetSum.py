3b00f39708adf71877ab271d183c2681724367bf
ShunaMae <shuna120700@gmail.com>
2024-01-04 17:25:33 +0800

    updated

---
+++ Daily_Practice/鉄則本/A18-SubsetSum.py
@@ -0,0 +1,14 @@
+N, S = map(int, input().split())
+A = list(map(int, input().split()))
+
+dp = [[False for _ in range(S+1)] for _ in range(N+1)]
+dp[0][0] = True
+
+for i in range(1, N+1):
+    for j in range(S+1):
+        dp[i][j] = dp[i-1][j]
+        if j >= A[i-1] and dp[i-1][j-A[i-1]]:
+            dp[i][j] = True
+
+if dp[-1][-1]: print("Yes")
+else:print("No")
