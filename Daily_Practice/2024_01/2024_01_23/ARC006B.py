0c386de68f930ffc5acf006f9fd4d8821df31f54
Pitta <shuna120700@gmail.com>
2024-01-23 14:30:25 +0800

    Update ARC006B.py

--- Daily_Practice/2024_01/2024_01_23/ARC006B.py
+++ Daily_Practice/2024_01/2024_01_23/ARC006B.py
@@ -1,26 +1,20 @@
-# https://atcoder.jp/contests/arc006/tasks/arc006_2
 
 N, L = map(int, input().split())
-G = [list(input()) for _ in range(L)]
-y = input()
+G = [input() for _ in range(L)]
+y = input()  # ゴールの位置
 
-print(G)
+goal = y.index("o")
 
+# 各開始位置からの経路を計算
 for pos in range(N):
-    cur = pos*2
+    cur = pos * 2
     for i in range(L):
-        # print(cur)
-        if cur > 0:
-            if G[i][cur-1] == "-":
-                cur -= 2
-        # print(G[i])
-        # print(G[i][cur+1])
-        if cur < (N)*2-1:
-            # print(G[i][cur+1], "sign")
-            if G[i][cur+1] == "-":
-                cur += 2
-    print("end")
+        if cur > 0 and G[i][cur-1] == "-":
+            cur -= 2
+        elif cur < N * 2 - 2 and G[i][cur+1] == "-":
+            cur += 2
 
-    
-    print(cur)
+    if cur == goal:
+        print(pos+1)
+        break
 
